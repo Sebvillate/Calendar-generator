8aaef6f029b3cb61aec6ec69eb89cc01e9b3533a
Sebastian Villate <48069442+Sebvillate@users.noreply.github.com>
2019-04-24 18:45:30 -0400

    Update Calendar.py

--- Calendar.py
+++ Calendar.py
@@ -1,7 +1,7 @@
 ##----------------------------------------------------------------------------------------------------------
 ##Program: Assignemt 4.A Calendar Program
 ##Author:Sebastian Villate
-##Date:April 18th
+##Date:April 24th
 ##Description: A program that recieves an input for the length of the month and the first day of the month,
 ##and generates a standard calendar within those parameters. The user inputs the length of month into the
 ##GetIntInput function, which checks for a valid integer input greater than or equal to 28 and less than 32.
@@ -9,52 +9,51 @@
 ##variable. Then the user inputs the first day of the month into the DayOfWeekCheck function, which checks for
 ##a valid input within a list named ValidDays. The function loops until a valid input is entered, and once it
 ##is entered, a integer depending on the day of the week is returned. This is to allign the number 1 on the calendar
-##With teh corresponding first day of the month. These two inputs are used to generate the calendar, where values
+##with the corresponding first day of the month. These two inputs are used to generate the calendar, where values
 ##between 1 and 31 (inclusive) are printed a maximum of rows of 7 and values outside of that range are
 ##replaced with 5 character spaces.
 ##Input: Length of month and first day of the month
 ##----------------------------------------------------------------------------------------------------------
 
+while True:
+    #Checks for a valid input for length of month and repeats loop until valid input is provided.
+    #When a valid input is entered, it is returned to the LengthOfMonth variable in the program
+    def GetIntInput(prompt,MinNumber,MaxNumber):
+        n=0
+        while MaxNumber<=n or n<MinNumber:
+            n=int(float(input(prompt)))
+            if MaxNumber>n and n>=MinNumber:
+                return n
+            else:
+                print()
+                print("Input is invalid, please enter a valid month length")
 
-#Checks for a valid input for length of month and repeats loop until valid input is provided.
-#When a valid input is entered, it is returned to the LengthOfMonth variable in the program
-def GetIntInput(prompt,MinNumber,MaxNumber):
-    n=0
-    while MaxNumber<=n or n<MinNumber:
-        n=int(float(input(prompt)))
-        if MaxNumber>n and n>=MinNumber:
-            return n
-        else:
-            print()
-            print("Input is invalid, please enter a valid month length")
-
-# Returns a different value depending on the first day of the month entered. This is done to
-#Allign the number 1 on the calendar with the day the month starts on, and to print blank spaces
-#Earlier in the week
-def DayOfWeekCheck(prompt):
-    ValidDays=['sun','mon','tue','wed','thr','fri','sat']
-    DayOfWeek=None
-    while DayOfWeek not in ValidDays:
-        DayOfWeek=input(prompt)
-        DayOfWeek=DayOfWeek.lower()
-        if DayOfWeek=="mon":
-            return 0
-        elif DayOfWeek=="tue":
-            return -1
-        elif DayOfWeek=="wed":
-            return -2
-        elif DayOfWeek=="thr":
-            return -3
-        elif DayOfWeek=="fri":
-            return -4
-        elif DayOfWeek=="sat":
-            return -5
-        elif DayOfWeek=="sun":
-            return 1
-        else:
-            print("Invalid input, please enter a day of the week")
-    
+    # Returns a different value depending on the first day of the month entered. This is done to
+    #Allign the number 1 on the calendar with the day the month starts on, and to print blank spaces
+    #Earlier in the week
+    def DayOfWeekCheck(prompt):
+        ValidDays=['sun','mon','tue','wed','thr','fri','sat']
+        DayOfWeek=None
+        while DayOfWeek not in ValidDays:
+            DayOfWeek=input(prompt).lower()
             
+            if DayOfWeek=="mon":
+                return 0
+            elif DayOfWeek=="tue":
+                return -1
+            elif DayOfWeek=="wed":
+                return -2
+            elif DayOfWeek=="thr":
+                return -3
+            elif DayOfWeek=="fri":
+                return -4
+            elif DayOfWeek=="sat":
+                return -5
+            elif DayOfWeek=="sun":
+                return 1
+            else:
+                print("Invalid input, please enter a day of the week")
+        
 #LengthOfMonth is assigned the valid month length returned by the MonthLengthCheck function
 LengthOfMonth=GetIntInput("How many days are there in the month?: ",28,32)
 #dow is assigned the integer value returned by the DayOfWeekCheck function
